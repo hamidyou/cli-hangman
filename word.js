0ac76ab290cd966056348957cb6ac103ea27a20c
Youssef <yhamid1982@gmail.com>
2018-10-12 22:59:25 -0400

    added package.json

--- word.js
+++ word.js
@@ -1,11 +1,11 @@
 const Letter = require('./letter')
-const k = require('kyanite/dist/kyanite')
+const { compose, reduce, map, split } = require('kyanite/dist/kyanite')
 const letter = x => new Letter(x)
 
 function Word (word) {
-  this.word = k.compose(k.map(x => letter(x)), k.split(''), word)
-  this.wordString = x => k.reduce((acc, char) => acc + char + ' ', '', k.map(x => x.guessedFn(), x))
-  this.checkLetter = (a, char) => k.map(x => x.checkFn(char), a)
+  this.word = compose(map(x => letter(x)), split(''), word)
+  this.wordString = x => reduce((acc, char) => acc + char + ' ', '', map(x => x.guessedFn(), x))
+  this.checkLetter = (a, char) => map(x => x.checkFn(char), a)
 }
 
 module.exports = Word
